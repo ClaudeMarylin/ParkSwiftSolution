4e77e863c7ae679d4a6fc2b5c1ee95c0999d119a
unknown <marylinbbs@gmail.com>
2024-04-02 13:28:27 +0200

    initialisation de la base de données dans le projet

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,41 @@
 import 'package:comparking/screens/authentif/WelcomePage.dart';
 import 'package:flutter/material.dart';
-
-
-
+import 'package:supabase/supabase.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+//initialisation de supabase
+  final String supabaseUrl;
+  final String supabaseKey;
+  final SupabaseClient supabaseClient;
+
+  MyApp({Key? key})
+      : supabaseUrl = 'https://ussrntnvxurlkukwhiej.supabase.co',
+        supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzc3JudG52eHVybGt1a3doaWVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg1MDU2MjQsImV4cCI6MjAyNDA4MTYyNH0.6k5Mhz5_aiFppamlP0X4A3v7DYDrKlrrMzGtu3RjYGM',
+        supabaseClient =
+            SupabaseClient('https://ussrntnvxurlkukwhiej.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzc3JudG52eHVybGt1a3doaWVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg1MDU2MjQsImV4cCI6MjAyNDA4MTYyNH0.6k5Mhz5_aiFppamlP0X4A3v7DYDrKlrrMzGtu3RjYGM'),
+        super(key: key);
+
+
+// comment utiliser le client dans la manipulation des bases de données/
+
+// final response = await client
+//     .from('ma_table')
+//     .select()
+//     .execute();
+// if (response.error != null) {
+//   // Gérer l'erreur
+// } else {
+//   final data = response.data;
+//   // Utiliser les données récupérées
+// }
+
+
+
+
 
   // This widget is the root of your application.
   @override
@@ -18,6 +43,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'ParkSwift',
         debugShowCheckedModeBanner: false,
-        home: WelcomePage ());
+        home: WelcomePage());
   }
 }
