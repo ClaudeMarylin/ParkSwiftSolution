25cad7c4034fb46da03fe68aa2750b87b56294a0
unknown <marylinbbs@gmail.com>
2024-04-23 23:39:36 +0200

    merge app alex+marylin

--- lib/screens/authentif/HomePage.dart
+++ lib/screens/authentif/HomePage.dart
@@ -99,27 +99,7 @@ class _HomePageState extends State<HomePage> {
   ];
 }
 
-// class DialogUtils {
-//   static void showLocationPermissionDeniedDialog(BuildContext context) {
-//     showDialog(
-//       context: context,
-//       builder: (BuildContext context) {
-//         return AlertDialog(
-//           title: Text("Accès à la localisation refusé"),
-//           content: Text("Pour utiliser cette application, veuillez activer l'accès à la localisation dans les paramètres de votre téléphone."),
-//           actions: [
-//             TextButton(
-//               onPressed: () {
-//                 Navigator.of(context).pop();
-//               },
-//               child: Text("Fermer"),
-//             ),
-//           ],
-//         );
-//       },
-//     );
-//   }
-// }
+
 
 class HomeTap extends StatefulWidget {
   const HomeTap({Key? key});
@@ -127,18 +107,7 @@ class HomeTap extends StatefulWidget {
   // get context => null;
 
   @override
-//   void initState() {
 
-//     requestLocationPermission();
-//   }
-
-//   Future<void> requestLocationPermission() async {
-//   final PermissionStatus status = await Permission.location.request();
-//   if (status == PermissionStatus.denied) {
-//   DialogUtils.showLocationPermissionDeniedDialog(context as BuildContext);
-
-//   }
-// }
 
   _HomeTapState createState() => _HomeTapState();
 }
@@ -256,96 +225,3 @@ class _HomeTapState extends State<HomeTap> {
   }
 }
 
-// class _HomeTapState extends State<HomeTap> {
-//   late GoogleMapController _mapController;
-//   CameraPosition _initialCameraPosition = const CameraPosition(
-//     target:
-//         LatLng(0, 0), // Coordonnées initiales de la carte (latitude, longitude)
-//     zoom: 12, // Zoom initial de la carte
-//   );
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _getCurrentLocation();
-//   }
-
-//   Future<void> _getCurrentLocation() async {
-//     Position? currentPosition = await LocationService.getCurrentLocation();
-//     if (currentPosition != null) {
-//       setState(() {
-//         _initialCameraPosition = CameraPosition(
-//           target: LatLng(currentPosition.latitude, currentPosition.longitude),
-//           zoom: 12,
-//         );
-//       });
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return SafeArea(
-//       child: Scaffold(
-//           body: Stack(
-//         children: [
-//           GoogleMap(
-//             initialCameraPosition: _initialCameraPosition,
-//             mapType: MapType.normal,
-//             onMapCreated: (GoogleMapController controller) {
-//               setState(() {
-//                 _mapController = controller;
-//               });
-//             },
-//           ),
-// // GoogleMap(
-// //   initialCameraPosition: const CameraPosition(
-// //     target: LatLng(0, 0), // Coordonnées initiales de la carte (latitude, longitude)
-// //     zoom: 12, // Zoom initial de la carte
-// //   ),
-// //   mapType: MapType.normal, // Type de carte (normal, satellite, terrain, etc.)
-// //   onMapCreated: (GoogleMapController controller) {
-// //     // Fonction appelée lorsque la carte est créée
-// //     // Vous pouvez utiliser le contrôleur de carte pour effectuer des opérations sur la carte
-// //   },
-// // ),
-
-//           Column(
-//             children: [
-//               // Barre de recherche
-//               Padding(
-//                 padding:
-//                     const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-//                 child: Row(
-//                   children: [
-//                     Expanded(
-//                       child: CustomTextFormField(
-//                         hint: "Rechercher",
-//                         prefixIcon: IconlyBroken.search,
-//                         filled: true,
-//                         enabled: true,
-//                       ),
-//                     ),
-//                     GestureDetector(
-//                       onTap: () {
-//                         // Action à effectuer lors de l'appui sur l'icône de notification
-//                       },
-//                       child: const Padding(
-//                         padding: EdgeInsets.all(8.0),
-//                         child: Icon(
-//                           Icons.notifications,
-//                           color: Colors.black,
-//                           size: 30,
-//                         ),
-//                       ),
-//                     ),
-//                   ],
-//                 ),
-//               ),
-//               // Autre contenu de la page ici
-//             ],
-//           ),
-//         ],
-//       )),
-//     );
-//   }
-// }
--- lib/screens/menuBar/reservation.dart
+++ lib/screens/menuBar/reservation.dart
@@ -63,6 +63,7 @@ class Map extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+
       height: 300,
       color: sBlue,
     );
